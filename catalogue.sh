b545f01f3d15135f8e509887d47df06b74b031e3
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-13 00:33:27 +0530

    Adding Roboshop Project Stage 1

--- catalogue.sh
+++ catalogue.sh
@@ -1,42 +1,44 @@
 echo -e "\e[36m>>>>>>>> Create Catalogue Service <<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Mongodb Repo <<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Install Nodejs Repos <<<<<<<<\e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+dnf install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Creating Application User <<<<<<<<\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>  Deleting Old Content <<<<<<<<\e[0m"
-rm -rf /app
+rm -rf /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Creating Application Directory <<<<<<<<\e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Downloading Application Content <<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Extracting Application Content <<<<<<<<\e[0m"
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
+cd /app &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Downloading Nodejs Dependency <<<<<<<<\e[0m"
-npm install
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Installing Mongodb Client <<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Downloading Catalogue Schema <<<<<<<<\e[0m"
-mongo --host mongodb.rdevops57online.com </app/schema/catalogue.js
+mongo --host mongodb.rdevops57online.com </app/schema/catalogue.js &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Starting Catalogue Service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
+
+
 
