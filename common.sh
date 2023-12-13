93d3b54f89913bf9f3afa50d838f4af26816abf6
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-13 22:11:19 +0530

    Adding Roboshop Project Stage 1

--- common.sh
+++ common.sh
@@ -1,6 +1,4 @@
 nodejs () {
-
-}
 echo -e "\e[36m>>>>>>>> Create ${component} Service <<<<<<<<\e[0m"
 cp ${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
 
@@ -12,7 +10,7 @@ dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 dnf install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[36m>>>>>>>> Creating Application ${component} <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> Creating Application user <<<<<<<<\e[0m"
 ${component}add roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>  Deleting Old Content <<<<<<<<\e[0m"
@@ -36,9 +34,10 @@ echo -e "\e[36m>>>>>>>> Installing Mongodb Client <<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Downloading ${component} Schema <<<<<<<<\e[0m"
-mongo --host mongodb.rdevops57online.com </app/schema/${component}.js &>>/tmp/roboshop.log
+mongo --host mongodb.rdevops57online.com </app/schema/user.js &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Starting ${component} Service <<<<<<<<\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable ${component} &>>/tmp/roboshop.log
 systemctl start ${component} &>>/tmp/roboshop.log
+}
