943a6da153cfec4849128fa86d7d08089795754b
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-13 21:58:13 +0530

    Adding Roboshop Project Stage 1

--- catalogue.sh
+++ catalogue.sh
@@ -1,44 +1,4 @@
-echo -e "\e[36m>>>>>>>> Create Catalogue Service <<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+component=catalogue
+source common.sh
 
-echo -e "\e[36m>>>>>>>> Mongodb Repo <<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Install Nodejs Repos <<<<<<<<\e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
-dnf install nodejs -y &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Creating Application User <<<<<<<<\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>>  Deleting Old Content <<<<<<<<\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Creating Application Directory <<<<<<<<\e[0m"
-mkdir /app &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Downloading Application Content <<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Extracting Application Content <<<<<<<<\e[0m"
-cd /app &>>/tmp/roboshop.log
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
-cd /app &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Downloading Nodejs Dependency <<<<<<<<\e[0m"
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Installing Mongodb Client <<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Downloading Catalogue Schema <<<<<<<<\e[0m"
-mongo --host mongodb.rdevops57online.com </app/schema/catalogue.js &>>/tmp/roboshop.log
-
-echo -e "\e[36m>>>>>>>> Starting Catalogue Service <<<<<<<<\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl start catalogue &>>/tmp/roboshop.log
-
-
-
+nodejs
---
+++ common.sh
@@ -0,0 +1,44 @@
+nodejs () {
+
+}
+echo -e "\e[36m>>>>>>>> Create ${component} Service <<<<<<<<\e[0m"
+cp ${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Mongodb Repo <<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Install Nodejs Repos <<<<<<<<\e[0m"
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+dnf install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Creating Application ${component} <<<<<<<<\e[0m"
+${component}add roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>>  Deleting Old Content <<<<<<<<\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Creating Application Directory <<<<<<<<\e[0m"
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Downloading Application Content <<<<<<<<\e[0m"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Extracting Application Content <<<<<<<<\e[0m"
+cd /app &>>/tmp/roboshop.log
+unzip /tmp/${component}.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Downloading Nodejs Dependency <<<<<<<<\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Installing Mongodb Client <<<<<<<<\e[0m"
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Downloading ${component} Schema <<<<<<<<\e[0m"
+mongo --host mongodb.rdevops57online.com </app/schema/${component}.js &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>> Starting ${component} Service <<<<<<<<\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable ${component} &>>/tmp/roboshop.log
+systemctl start ${component} &>>/tmp/roboshop.log
