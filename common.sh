016b86a325c43fa74c3bec4e3ca289bedd2baada
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-14 14:21:50 +0530

    Adding Roboshop Project Stage 1

--- common.sh
+++ common.sh
@@ -1,6 +1,9 @@
 log=tmp/roboshop.log
 
 func_appreq() {
+  echo -e "\e[36m>>>>>>>> Create ${component} Service <<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service app &>>${log}
+
   echo -e "\e[36m>>>>>>>> Create Application User <<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
 
@@ -26,11 +29,26 @@ func_systemd() {
   systemctl restart ${component} &>>${log}
 }
 
+func_schema_setup() {
+ if [ "${schema_type}" == "mongodb" ]; then
+    echo -e "\e[36m>>>>>>>> Install Mongo Client <<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    yum instal mongodb-org-shell -y &>>${log}
+    echo -e "\e[36m>>>>>>>> Load User Schema <<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+    mongo --host mongodb.rdevops57online.com </app/schema/${component}.js &>>${log}
+ fi
+
+ if ["${schema_type}" == "mysql"]; then
+    echo -e "\e[36m>>>>>>>> Install Mysql Client <<<<<<<<\e[0m"
+    yum install mysql -y &>>${log}
+
+    echo -e "\e[36m>>>>>>>> Load Schema <<<<<<<<\e[0m"
+    mysql -h mysql.rdevops57online.com -uroot -pRoboshop@1 </app/schema/${component}.sql &>>${log}
+ fi
+
+}
+
 func_nodejs() {
   log=/tmp/roboshop.log
-
-  echo -e "\e[36m>>>>>>>> Create ${component} Service <<<<<<<<\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
 
   echo -e "\e[36m>>>>>>>> Create Mongodb Repo <<<<<<<<\e[0m"
   cp mongo.rep /etc/yum.repos.d/mongo.repo &>>${log}
@@ -46,19 +64,12 @@ func_nodejs() {
   echo -e "\e[36m>>>>>>>> Download NodeJS Dependency <<<<<<<<\e[0m"
   npm install &>>${log}
 
-  echo -e "\e[36m>>>>>>>> Install Mongo Client <<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  yum install mongodb-org-shell -y &>>${log}
-
-  echo -e "\e[36m>>>>>>>> Load User Schema <<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  mongo --host mongodb.rdevops57online.com </app/schema/${component}.js &>>${log}
+  func_schema_setup
 
   func_systemd
 }
 
 func_java() {
- echo -e "\e[36m>>>>>>>> Create ${component} Service <<<<<<<<\e[0m"
- cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-
  echo -e "\e[36m>>>>>>>> Install Maven <<<<<<<<\e[0m"
  yum install maven -y &>>${log}
 
@@ -68,10 +79,18 @@ func_appreq
  mvn clean package &>>${log}
  mv target/${component}-1.0.jar ${component}.jar
 
- echo -e "\e[36m>>>>>>>> Install Mysql Client <<<<<<<<\e[0m"
- yum install mysql -y &>>${log}
+ func_systemd
+}
 
- echo -e "\e[36m>>>>>>>> Load Schema <<<<<<<<\e[0m"
- mysql -h mysql.rdevops57online.com -uroot -pRoboshop@1 </app/schema/${component}.sql &>>${log}
+ func_python() {
+
+ cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+ yum install python36 gcc python3-devil -y &>>${log}
+
+ func_appreq
+
+ echo -e "\e[36m>>>>>>>> Build ${component} Service <<<<<<<<\e[0m"
+ pip3.6 install -r requirement.txt &>>${log}
+
  function_systemd
 }
